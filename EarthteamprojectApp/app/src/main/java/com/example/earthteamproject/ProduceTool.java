ca066cfc6a833bd1ce0e89b51a5a53235801c680
NateTRead <64865653+NateTRead@users.noreply.github.com>
2020-06-10 02:27:53 -0700

    Added some documentation to ProduceTool code

--- EarthteamprojectApp/app/src/main/java/com/example/earthteamproject/ProduceTool.java
+++ EarthteamprojectApp/app/src/main/java/com/example/earthteamproject/ProduceTool.java
@@ -9,37 +9,51 @@ import java.util.*;
 public class ProduceTool {
 
     public static void main(String[] args) throws IOException {
+
+
         String rawFile = "";
-        String[] parsedProduceString = new String[5];
-        Produce[] produceOBJArray = new Produce[10];
+        String[] parsedProduceString;
         FileOutputStream outStream = null;
         ObjectOutputStream objStream = null;
         Scanner inScanner = new Scanner(System.in);
         System.out.println("Enter the absolute path for input text file:");
         File inFile = new File(inScanner.nextLine());
+        System.out.println("Enter the amount of items to be parsed:");
+        Produce[] produceOBJArray = new Produce[Integer.parseInt(inScanner.nextLine())];
         System.out.println("File opened successfully");
         Scanner fileScanner = new Scanner(inFile);
 
+        //Loading text from file
         while (fileScanner.hasNext()) {
             rawFile = rawFile + fileScanner.nextLine();
         }
 
 
-        String[] unparsedProduce = rawFile.split("\\|");
-        for (int i = 0; i < unparsedProduce.length; i++) {
-            parsedProduceString = unparsedProduce[i].split("}");
+        //This will split up multiple produce objects within one text file and add their info to an array
+        String[] rawProduce = rawFile.split("\\|");
+
+        //This creates a produce object for each item in rawProduce[]
+        for (int i = 0; i < rawProduce.length; i++) {
+
+            //splitting up the info fields by the "}" delimiting char
+            parsedProduceString = rawProduce[i].split("}");
+
+            //this could probably be one statement but my brain is smooth
             String name = parsedProduceString[0];
             String descShort = parsedProduceString[1];
             String descLong = parsedProduceString[2];
             String imgURL = parsedProduceString[3];
             Double date = Double.parseDouble(parsedProduceString[4]);
+
+            //populating array with new produce items
             produceOBJArray[i] = new Produce(name, descLong, descShort, imgURL, date);
         }
 
         System.out.println("Objects successfully parsed from file");
 
 
-        for (int i = 0; i < unparsedProduce.length; i++)
+        //saving to file
+        for (int i = 0; i < rawProduce.length; i++)
             try {
                 outStream = new FileOutputStream(produceOBJArray[i].getName() + ".bin");
                 objStream = new ObjectOutputStream(outStream);
